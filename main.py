ca0d2b046cc37fd36fc7fd0574d926f53f336b3f
RelicR <karratel73@gmail.com>
2022-05-08 02:22:54 +0400

    ~~

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ try:
     while n < 4:
         n = int(input("Задайте количество строк и столбцов > 3: "))
     k = int(input("Задайте значение коэффициента k: "))
-    A = np.random.randint(-10, 10, (n, n), dtype=int)
+    A = np.random.randint(-10.0, 10.0, (n, n), dtype='int64')
     """A = np.zeros((n, n))
     for i in range(n):
         for j in range(n):
@@ -21,15 +21,15 @@ try:
                 A[i][j] = (n - i) * 10 + j"""
     F = np.copy(A)
     np.set_printoptions(precision=2, linewidth=200)
-    print(f"----A----\n{A}\n----F----\n\n{F}\n")
+    print(f"----A----\n{A}\n\n----F----\n{F}\n\n")
     cond_e, cond_lines = 0, 1
     for i in range(n):
         for j in range(n):
             if i % 2 == 0:
-                cond_lines *= A[i][j]
+                cond_lines *= int(A[i][j])
             if i > (n // 2 - (n - 1) % 2) and j > (n // 2 - (n - 1) % 2) and j % 2 == 0 and A[i][j] == 0:
                 cond_e += 1
-    print(f"Количество нулей в нечетных столбцах Е = {cond_e}\nПроизведение чисел в нечетных строках = {cond_lines}")
+    print(f"Количество нулей в нечетных столбцах Е = {cond_e}\nПроизведение чисел в нечетных строках = {cond_lines}\n")
     if cond_e * k > cond_lines:
         for i in range(n // 2):
             F[i] = F[i][::-1]
@@ -42,12 +42,12 @@ try:
         if np.linalg.det(F) == 0:
             print("Матрица F - вырожденная, невозможно провести вычисления")
         else:
-            print(f"A*AT – K * F-1\n{np.matmul(A, np.transpose(A)) - np.linalg.inv(F) * k}")
+            print(f"----A*AT – K * F-1----\n{np.matmul(A, np.transpose(A)) - np.linalg.inv(F) * k}")
     else:
         if np.linalg.det(A) == 0:
             print("Матрица A - вырожденная, невозможно провести вычисления")
         else:
-            print(f"(A-1 +G-FТ)*K\n{(np.linalg.inv(A) + np.tril(A) - np.transpose(F)) * k}")
+            print(f"----(A-1 +G-FТ)*K----\n{(np.linalg.inv(A) + np.tril(A) - np.transpose(F)) * k}")
     print(f"Время выполнения: {time.process_time()}")
 
 except ValueError:
